3edd6b93731b1c487d2815ebac605a3cc8f93d6a
admond <admondtamang@gmail.com>
2021-11-15 10:52:25 +0545

    [add] redux added

--- App.js
+++ App.js
@@ -2,15 +2,29 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { LogBox } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
 import Navigation from "./src/navigation";
+
+import { PersistGate } from "redux-persist/integration/react";
+
+import { persistStore } from "redux-persist";
+import { QueryClient, QueryClientProvider } from "react-query";
+import store from "./src/redux/configureStore";
 
 export default function App() {
     LogBox.ignoreAllLogs(true);
-
+    const queryClient = new QueryClient();
+    let persistor = persistStore(store);
     return (
-        <SafeAreaProvider>
-            <StatusBar style={"dark"} />
-            <Navigation />
-        </SafeAreaProvider>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <SafeAreaProvider>
+                        <StatusBar style={"dark"} />
+                        <Navigation />
+                    </SafeAreaProvider>
+                </PersistGate>
+            </Provider>
+        </QueryClientProvider>
     );
 }
